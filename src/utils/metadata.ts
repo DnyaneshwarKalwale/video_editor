5e691c55e380013961813d01fad9a5045f09612d
DnyaneshwarKalwale <prembkalwale@gmail.com>
2025-08-11 17:43:11 +0530

    video editor

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -28,4 +28,4 @@ export function createMetadata(override: Metadata): Metadata {
 export const baseUrl =
 	process.env.NODE_ENV === "development"
 		? new URL("http://localhost:3000")
-		: new URL("ignite.wantace.com");
+		: new URL("https://ignite.wantace.com");
