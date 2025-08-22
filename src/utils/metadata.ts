3b3bdb1075d94a66c4bf52f7eb6a89e2c12726ce
DnyaneshwarKalwale <prembkalwale@gmail.com>
2025-08-22 09:48:14 +0530

    bugs and UI changes

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -24,7 +24,7 @@ export function createMetadata(override: Metadata): Metadata {
 		},
 	};
 }
-
+//ignite
 export const baseUrl =
 	process.env.NODE_ENV === "development"
 		? new URL("http://localhost:3000")
