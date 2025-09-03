604598bd119506bb6d46d800ce550387709b19b0
DnyaneshwarKalwale <prembkalwale@gmail.com>
2025-09-03 11:50:31 +0530

    new changes

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -28,4 +28,4 @@ export function createMetadata(override: Metadata): Metadata {
 export const baseUrl =
 	process.env.NODE_ENV === "development"
 		? new URL("http://localhost:3000")
-		: new URL("https://ignite.wantace.com");
+		: new URL("editor.scalez.in");
